15f13b700cbdeaa8e4a53e64735ffee72e51b104
Issac Kelly <issac.kelly@gmail.com>
2011-01-27 11:42:16 -0500

    South migration to support model changes

--- biblion/models.py
+++ biblion/models.py
@@ -41,7 +41,7 @@ class Section(models.Model):
 
 class Post(models.Model):
     
-    section = models.ManyToManyField(Section, related_name="posts", default=1)
+    sections = models.ManyToManyField(Section, related_name="posts", default=1)
     
     title = models.CharField(max_length=90)
     slug = models.SlugField()
